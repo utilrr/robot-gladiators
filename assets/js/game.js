3b69259535dc32d799118ad643f270fb1ce5314f
Mark J. Healy <gmarkghealy@gmail.com>
2022-01-17 20:03:10 -0500

    added debugger

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,25 +5,24 @@ var playerMoney = 10;
 
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-// console.log(enemyNames);
-// console.log('Roborto');
-// console.log('Amy Android');
-// console.log('Robo Trumble');
-// console.log(enemyNames.length);
-// for(var i = 0; i < enemyNames.length; i++) {
-//     console.log(enemyNames[i]);
-//     console.log(i);
-//     console.log(enemyNames[i] + " is at " + i + " index");
-//   }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+      // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+    // place fight function code block here . . .
+    for(var i = 0; i < enemyNames.length; i++) {
+        debugger;
+        fight(enemyNames[i]);
+      }
+  }
+
+
+   
   
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -76,6 +75,4 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
 
 // run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+
